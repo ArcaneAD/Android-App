415ffd1cd173a8de871ba5a87d69425d810cf9d6
ArcaneAD <161540867+ArcaneAD@users.noreply.github.com>
2024-02-28 04:45:11 +0300

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bindingClass : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+    }
+    fun onClickWorkActivity(view : View){
+
+        val intent = Intent(this, WorkActivity::class.java)
+        startActivity(intent)
+    }
+}
